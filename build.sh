50044eadfbbb31b4aa736306ff3dbe172059cf7e
Nicolas CARPi <nico-git@deltablot.email>
2020-05-02 05:04:48 +0200

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 cd src || exit 1
-docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/builder:3.8 jekyll build
+# install js dependencies
+docker run --rm -v "$PWD:/usr/src/app" -w /usr/src/app -it node:current-alpine yarn install
+# build minified assets
+./node_modules/.bin/grunt
+# build website
+docker run --rm -v "$PWD:/srv/jekyll" -it jekyll/builder:3.8 jekyll build
